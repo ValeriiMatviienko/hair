ec992fa8da32f4aa4ab6fa03779928736bcdb25f
Valerii Matviienko <valerii.matviienko@capgemini.com>
2023-09-23 22:55:19 +0200

    fixed footer styles

---
+++ app/components/Footer/SocialLinks.tsx
@@ -0,0 +1,15 @@
+import { SocialLinks } from "@/app/types/types";
+import { FacebookIcon, InstagramIcon } from "@/public/images/Footer/icons";
+
+export const socialLinks: SocialLinks[] = [
+  {
+    Component: FacebookIcon,
+    link: "https://facebook.com",
+    description: "Facebook",
+  },
+  {
+    Component: InstagramIcon,
+    link: "https://instagram.com/hair.by.hanna.wroclaw",
+    description: "Instagram",
+  },
+];
