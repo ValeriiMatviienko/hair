194d5b4b8f28e49cf2c8389eabd9f9888dfec1f2
Valerii Matviienko <valerii.matviienko@capgemini.com>
2023-10-22 14:41:23 +0200

    added swipe by finger

--- app/components/Gallery/Slider.tsx
+++ app/components/Gallery/Slider.tsx
@@ -7,11 +7,24 @@ import { SliderProps } from "@/app/types/types";
 const Slider: React.FC<SliderProps> = ({ images }) => {
   const length = images.length - 1;
   const [index, setIndex] = useState<number>(0);
+  const [startX, setStartX] = useState<number>(0);
+  const [distance, setDistance] = useState<number>(0);
+
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    setStartX(e.touches[0].clientX);
+  };
+  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    setDistance(e.touches[0].clientX - startX);
+  };
+  const handleTouchEnd = () => {
+    distance > 50 ? handlePrev() : distance < -50 && handleNext();
+    /** Reset the distance for the next swipe */
+    setDistance(0);
+  };
 
   const handlePrev = () => {
     index === 0 ? setIndex(length) : setIndex(index - 1);
   };
-
   const handleNext = () => {
     index === length ? setIndex(0) : setIndex(index + 1);
   };
@@ -20,7 +33,7 @@ const Slider: React.FC<SliderProps> = ({ images }) => {
     <div className="flex flex-col items-center justify-center">
       <div className="flex items-center justify-between w-full px-4">
         <ArrowLeftIcon
-          className="flex-shrink-0 w-8 h-8 text-black cursor-pointer"
+          className="w-8 h-8 text-black cursor-pointer"
           onClick={handlePrev}
         />
         <Image
@@ -29,18 +42,23 @@ const Slider: React.FC<SliderProps> = ({ images }) => {
           width={images[index].width}
           height={images[index].height}
           className="max-w-full"
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          onTouchEnd={handleTouchEnd}
         />
         <ArrowRightIcon
-          className="flex-shrink-0 w-8 h-8 text-black cursor-pointer"
+          className="w-8 h-8 text-black cursor-pointer "
           onClick={handleNext}
         />
       </div>
-      <div className="flex mt-4 space-x-2">
+      <div className="flex items-center mt-4 space-x-3">
         {images.map((_, i) => (
           <div
             onClick={() => setIndex(i)}
             key={i}
-            className="w-2 h-2 rounded-full cursor-pointer"
+            className={`${
+              i === index ? "w-4 h-4" : "w-2 h-2"
+            } rounded-full cursor-pointer bg-softgreen`}
           ></div>
         ))}
       </div>
