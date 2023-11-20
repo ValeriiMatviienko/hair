5ed4698d9960eaa9b3ffedfc0a81d712e616ade0
Valerii Matviienko <valerii.matviienko@capgemini.com>
2023-11-20 11:27:40 +0100

    added translation and fix accordeon icon

--- app/components/FAQ/Accordion.tsx
+++ app/components/FAQ/Accordion.tsx
@@ -18,14 +18,18 @@ const Accordion = ({ items }: AccordionProps) => {
             className="flex items-center justify-between p-4 cursor-pointer"
             onClick={() => handleAccordionClick(index)}
           >
-            <p className="font-semibold text-black md:text-xl">
-              {item.question}
-            </p>
-            {openIndex === index ? (
-              <ChevronUpIcon className="w-6 h-6 text-black" />
-            ) : (
-              <ChevronDownIcon className="w-6 h-6 text-black" />
-            )}
+            <div className="flex-grow">
+              <p className="font-semibold text-black md:text-xl">
+                {item.question}
+              </p>
+            </div>
+            <div>
+              {openIndex === index ? (
+                <ChevronUpIcon className="w-6 h-6 text-black" />
+              ) : (
+                <ChevronDownIcon className="w-6 h-6 text-black" />
+              )}
+            </div>
           </div>
           {openIndex === index && (
             <div className="p-4">
