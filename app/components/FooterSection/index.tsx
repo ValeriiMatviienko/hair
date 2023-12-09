94f60f2d74762814b00ecd7dd6ad7948675c9265
Valerii Matviienko <valerii.matviienko@capgemini.com>
2023-12-09 18:44:20 +0100

    fixes and refactor

--- app/components/FooterSection/index.tsx
+++ app/components/FooterSection/index.tsx
@@ -37,8 +37,12 @@ const Footer = () => {
               {t("adress")}:
             </h2>
             <div className="text-lg text-center hover:underline ">
-              <a href="" target="_blank" rel="noopener noreferrer">
-                SOON!
+              <a
+                href="https://www.google.com/maps/search/?api=1&query=Aleja+Armii+Krajowej+48+Wrocław+(Krzyki)"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                al. Armii Krajowej 48 <br /> Wrocław (Krzyki)
               </a>
             </div>
           </div>
---
+++ app/components/Navbar/LogoComponent.tsx
@@ -0,0 +1,11 @@
+const LogoComponent = () => {
+  return (
+    <div className="flex items-center flex-shrink-0 text-center">
+      <a href="/" className="text-3xl font-semibold text-black ">
+        Hair by Hanna
+      </a>
+    </div>
+  );
+};
+
+export default LogoComponent;
