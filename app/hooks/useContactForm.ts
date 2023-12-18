98e458030bd1e3e64984ee482e73c92229a49466
Valerii Matviienko <valerii.matviienko@capgemini.com>
2023-12-18 15:20:17 +0100

    code refactor

--- app/hooks/useContactForm.ts
+++ app/hooks/useContactForm.ts
@@ -6,13 +6,13 @@ import { useNavigationContext } from "../context/NavigationContext";
 
 const useContactForm = () => {
   const t = useTranslations("Index");
+  const { isContactFormOpen, setIsContactFormOpen } = useNavigationContext();
   const [inputValues, setInputValues] = useState<InputType>({
     nameInput: "",
     numberInput: "",
     descriptionInput: "",
   });
-  const [showToastContainer, setShowToastContainer] = useState<boolean>(false);
-  const { setIsContactFormOpen } = useNavigationContext();
+  const [isNumberValid, setIsNumberValid] = useState<boolean>(true);
 
   const resetInputs = useCallback(() => {
     setInputValues({
@@ -21,6 +21,7 @@ const useContactForm = () => {
       descriptionInput: "",
     });
   }, []);
+
   const toggleModal = useCallback(
     ({ open, setIsOpen }: ToggleModalProps) => {
       !open && resetInputs();
@@ -29,38 +30,77 @@ const useContactForm = () => {
     [resetInputs]
   );
 
-  const handleSubmitForm = (setIsOpen: (isOpen: boolean) => void) => {
-    fetch("/api/sendEmail", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: inputValues.nameInput,
-        number: inputValues.numberInput,
-        message: inputValues.descriptionInput,
-      }),
-    })
-      .then((response) => response.json())
-      .then((_data) => {
-        toast.success(t("messageSent"));
-        setShowToastContainer(true);
-        setIsContactFormOpen(false);
+  const handleSubmitForm = useCallback(
+    (setIsOpen: (isOpen: boolean) => void) => {
+      fetch("/api/sendEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: inputValues.nameInput,
+          number: inputValues.numberInput,
+          message: inputValues.descriptionInput,
+        }),
       })
-      .catch(() => {
-        toast.error(t("messageError"));
-        setShowToastContainer(true);
-      });
+        .then((response) => response.json())
+        .then((_data) => {
+          toast.success(t("messageSent"));
+          setIsContactFormOpen(false);
+        })
+        .catch(() => {
+          toast.error(t("messageError"));
+        });
 
+      resetInputs();
+      toggleModal({ open: false, setIsOpen });
+    },
+    [inputValues, resetInputs, setIsContactFormOpen, t, toggleModal]
+  );
+
+  const handleChange = useCallback(
+    (e: { target: { name: string; value: string } }) => {
+      const { name, value } = e.target;
+      if (name === "numberInput") {
+        const isValid = /^\d*$/.test(value);
+        setIsNumberValid(isValid);
+        if (!isValid) return;
+      }
+      setInputValues((prevState) => ({ ...prevState, [name]: value }));
+    },
+    [setInputValues]
+  );
+
+  const isDisabled = Object.values(inputValues).some((value) => value === "");
+
+  const handleFormSubmittion = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      handleSubmitForm(setIsContactFormOpen);
+    },
+    [handleSubmitForm, setIsContactFormOpen]
+  );
+
+  const toggleModalOpen = useCallback(() => {
+    setIsContactFormOpen(true);
+  }, [setIsContactFormOpen]);
+
+  const toggleModalClose = useCallback(() => {
     resetInputs();
-    toggleModal({ open: false, setIsOpen });
-  };
+    setIsNumberValid(true);
+    setIsContactFormOpen(false);
+  }, [resetInputs, setIsContactFormOpen]);
 
   return {
     inputValues,
-    setInputValues,
     handleSubmitForm,
-    toggleModal,
+    handleChange,
+    isDisabled,
+    handleFormSubmittion,
+    toggleModalOpen,
+    toggleModalClose,
+    isNumberValid,
+    isContactFormOpen,
   };
 };
 
