1654292f4d2157c241793299baf9bc7bb7191366
Valerii Matviienko <valerii.matviienko@capgemini.com>
2024-05-24 19:18:17 +0200

    fixed robots

---
+++ app/components/GoogleAnalytics/index.tsx
@@ -0,0 +1,25 @@
+import Script from "next/script";
+import React from "react";
+
+const GoogleAnalytics = () => (
+  <>
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+    />
+    <Script
+      id="google-analytics"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GTM_ID}');
+        `,
+      }}
+    />
+  </>
+);
+
+export default GoogleAnalytics;
--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,17 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: "/private/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/private/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+    ],
     sitemap: "https://hairbyhanna.pl/sitemap.xml",
   };
 }
