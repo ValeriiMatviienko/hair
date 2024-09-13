63745c9a95a2ea5ca3fdb9751944ac1134124456
Valerii Matviienko <valerii.matviienko@capgemini.com>
2024-09-13 17:00:51 +0200

    price change

--- app/components/PriceSection/ProductPriceItems.tsx
+++ app/components/PriceSection/ProductPriceItems.tsx
@@ -10,20 +10,20 @@ export const getProductPrices = (t: TranslationFunction): ProductPrice[] => [
     name: t("price_nanoplastia"),
     shortHairPrice: 350,
     midHairPrice: 470,
-    longHairPrice: 550,
+    longHairPrice: 570,
   },
   {
     name: t("price_keratine"),
-    shortHairPrice: 300,
-    midHairPrice: 370,
-    longHairPrice: 460,
+    shortHairPrice: 330,
+    midHairPrice: 380,
+    longHairPrice: 480,
   },
 
   {
     name: t("price_botox"),
-    shortHairPrice: 270,
-    midHairPrice: 330,
-    longHairPrice: 420,
+    shortHairPrice: 300,
+    midHairPrice: 350,
+    longHairPrice: 450,
   },
 ];
 
@@ -31,9 +31,9 @@ export const getAdditionalServices = (
   t: TranslationFunction
 ): AdditionalService[] => [
   { name: t("trichological_consultation"), price: 250 },
-  { name: t("bio_ampoule"), price: 50 },
-  { name: t("scalp_peeling"), price: 70 },
-  { name: t("hair_trimming"), price: 90 },
+  { name: t("bio_ampoule"), price: 150 },
+  { name: t("scalp_peeling"), price: 250 },
+  { name: t("hair_trimming"), price: 120 },
   { name: t("sos_treatment"), price: 50 },
-  { name: t("hair_polishing"), price: 120 },
+  { name: t("hair_polishing"), price: 150 },
 ];
