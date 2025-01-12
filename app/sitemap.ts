2d1e331e1cf6489f992d10f55af098a8934dfa22
Valerii Matviienko <valeriimatviienko@gmail.com>
2025-01-12 14:09:52 +0100

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,23 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const currentDate = new Date();
   return [
     {
       url: "https://hairbyhanna.eu",
-      lastModified: new Date(),
+      lastModified: currentDate,
+    },
+    {
+      url: "https://hairbyhanna.eu/en",
+      lastModified: currentDate,
+    },
+    {
+      url: "https://hairbyhanna.eu/pl",
+      lastModified: currentDate,
+    },
+    {
+      url: "https://hairbyhanna.eu/ua",
+      lastModified: currentDate,
     },
   ];
 }
