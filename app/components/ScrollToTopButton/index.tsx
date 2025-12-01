cea160b09ad20878bca62313450c2154377854ec
Valerii Matviienko <valeriimatviienko@gmail.com>
2025-12-01 21:37:08 +0100

    slider update

--- app/components/ScrollToTopButton/index.tsx
+++ app/components/ScrollToTopButton/index.tsx
@@ -22,7 +22,7 @@ const ScrollToTopButton: FC = () => {
 
   return isVisible ? (
     <div
-      className="fixed p-2 rounded-full cursor-pointer bg-opacity-20 bg-softgreen bottom-4 right-4"
+      className="fixed p-2 rounded-full cursor-pointer bg-opacity-20 bg-gray-100/25 bottom-4 right-4"
       onClick={scrollToTop}
     >
       <FaArrowUp className="w-6 h-6 sm:w-10 sm:h-10 text-darkgreen" />
